b7c5db0a042e8c84c48170f3b382452115031452
ivanpetkov219 <51496032+vankata2001@users.noreply.github.com>
2020-06-27 10:50:41 +0300

    exam init

---
+++ Java Web/Exam27062020/src/main/java/exam/init/DataInit.java
@@ -0,0 +1,18 @@
+package exam.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInit implements CommandLineRunner {
+
+    //todo inject the service for initializing
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        //this.service.initData();
+    }
+}
